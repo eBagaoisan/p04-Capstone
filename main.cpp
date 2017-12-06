bafc022c9cf25887b1e7247a59ded64a5ec2203d
ebagaoisan <ebagaoisan1@mail.csuchico.edu>
2017-12-05 20:37:43 -0800

    Started with ofstream for game inventory

--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
 
+using namespace std;
+
+int main()
+{   
+    
+
+    //-------------------------------------------------------
+
+    //Player inventory
+    string item;
+    ofstream ofs;
+    ofs.open("inventory.txt");
+    cout<<"Enter: ";
+    cin>>item;
+    ofs<<item;
+    ofs.close();
+    
+    return 0;
+}
